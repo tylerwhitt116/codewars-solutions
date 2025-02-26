53747ddd15c2fa10e3498cfe1ab25c48761e6499
Tyler Rivera-Whitt <triverawhitt@gmail.com>
2025-02-25 20:57:15 -0500

    Add solution - CodeHub

---
+++ 8-kyu/sum-the-strings/sum-the-strings.js
@@ -0,0 +1,19 @@
+function sumStr(a,b) {
+// a and b are strings of a number
+  // convert a and b to actual numbers
+  //create the equation/sum of the two numbers
+  //return the sum as a string
+  
+  
+  let int1 = Number(a);
+  let int2 = Number(b);
+  
+  
+  let sum = int1 + int2;
+  
+  return sum.toString();
+  
+  
+  
+  
+}
