92fe1e1b652b5b6465105fdc90ccac3eb8722a37
Tyler Rivera-Whitt <triverawhitt@gmail.com>
2025-03-03 10:13:47 -0500

    Update solution - CodeHub

--- 7-kyu/beginner-series-number-3-sum-of-numbers/beginner-series-number-3-sum-of-numbers.js
+++ 7-kyu/beginner-series-number-3-sum-of-numbers/beginner-series-number-3-sum-of-numbers.js
@@ -1,3 +1,4 @@
+ 
 function getSum(a, b)
 {
   if(a === b) return a || b;  
@@ -5,5 +6,5 @@ function getSum(a, b)
   let min = Math.min(a,b);
   let max = Math.max(a,b);
   
-  return ((max - min + 1) (max + min)) / 2
+  return ((max - min + 1) * (max + min)) / 2
 }
