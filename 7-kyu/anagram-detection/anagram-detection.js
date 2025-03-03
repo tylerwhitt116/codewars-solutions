c1047a4bd3509f6bb2bcc3346996d2a2ae338554
Tyler Rivera-Whitt <triverawhitt@gmail.com>
2025-03-02 19:55:09 -0500

    Update solution - CodeHub

--- 7-kyu/anagram-detection/anagram-detection.js
+++ 7-kyu/anagram-detection/anagram-detection.js
@@ -1,8 +1,7 @@
- 
 // write the function isAnagram
 var isAnagram = function(test, original) {
-  let sort1 = [test.split(' ').sort().toLowerCase()];
-  let sort2 = original.sort().toLowerCase();
-  return sort1;
-};
+   
+return test.toLowerCase().split('').sort().join('') === original.toLowerCase().split('').sort().join('');
+​
+}
 ​
