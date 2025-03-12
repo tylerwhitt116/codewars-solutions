04c162cb6eee89c907a5cba7521cf67e9668a9c3
Tyler Rivera-Whitt <triverawhitt@gmail.com>
2025-03-12 13:37:57 -0400

    Update solution - CodeHub

--- 7-kyu/vowel-count/vowel-count.py
+++ 7-kyu/vowel-count/vowel-count.py
@@ -1,8 +1,8 @@
-def get_count(str):
-    vowels = ['a', 'e', 'i', 'o', 'u']
+def get_count(sentence):
+    vowels = ["a", "e", "i", "o", "u"]
     count = 0
-    
-    for i in str:
+    for i in sentence:
         if i in vowels:
-            count +=1
+            count+=1
     return count
+​
